ab917c952cfcaf56d5b45a108d5108563879f79b
DineshChirnanchu <Dinesh516ch@outlook.com>
2020-04-18 12:44:32 +0530

    Included missing projects to make the test inputs and results same as the ld vstests

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -12,19 +12,19 @@ namespace UnitTestProject1
             Assert.IsTrue(true, "It shouldn't fail");
         }
 
-        [TestMethod, Priority(0), Owner("test")]
+        [TestMethod, Priority(2), Owner("test")]
         public void TestRunParameterTest()
         {
             Assert.AreEqual("DataSource=tcp:ServerName.database.windows.net,1433;InitialCatalog=DatabaseName", this.TestContext.Properties["connectionString"], "Test run parameter not overridden. Should fail unless overridden");
         }
 
-        [TestMethod, Priority(0)]
+        [TestMethod]
         public void TestRunParameterOverrideParam1Value()
         {
             Assert.AreEqual("OverrideParam1Value", this.TestContext.Properties["param1"], "Value for param1 doesnt match the expected overriden value : OverrideParam1Value");
         }
 
-        [TestMethod, Priority(0)]
+        [TestMethod]
         public void TestRunParameterNoOverrideParam2Value()
         {
             Assert.AreEqual("Param2Value", this.TestContext.Properties["param2"], "Value for param2 doesnt match the expected original value : Param2Value");
@@ -37,19 +37,19 @@ namespace UnitTestProject1
             Assert.IsTrue(false, "This shouldn't be executed");
         }
 
-        [TestMethod, Priority(0), Owner("test")]
+        [TestMethod, Priority(3), Owner("test")]
         public void P3Test()
         {
             Assert.IsTrue(true, "Testing with Priority");
         }
 
-        [TestMethod, Priority(0), Owner("test"), TestCategory("TestCat")]
+        [TestMethod, Priority(2), Owner("test"), TestCategory("TestCat")]
         public void TestCategoryTest()
         {
             Assert.IsTrue(true, "Testing with some category");
         }
 
-        [TestMethod, Priority(0)]
+        [TestMethod]
         public void FailingTest()
         {
             Assert.IsTrue(true, "Another simple test which throws exception");
